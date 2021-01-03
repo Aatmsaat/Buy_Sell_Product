13ec89f8dd540ea635fa471fabe758e76f0bc0d7
Vaibhav Gupta <47090589+Aatmsaat@users.noreply.github.com>
2021-01-03 18:29:18 +0530

    buy file for the project
    code can be easily understood

---
+++ buy.py
@@ -0,0 +1,100 @@
+import sqlite3
+conn = sqlite3.connect('my_database.db')
+c = conn.cursor()
+
+# function to change users city
+
+def change_city(uid, buyercity, buyerstate):
+    print("\nchoose one of cities products available for your state", buyerstate)
+    print("-"*86)
+    c.execute(f"SELECT user.city FROM user INNER JOIN product on product.uid = user.uid WHERE state = '{buyerstate}' AND city != '{buyercity}'")
+    cities = set(c.fetchall())
+    cities = [*cities]
+    if not cities:
+        print('\nUnfortunately there no products except in your city\n')
+        return (True, buyercity)
+    
+    # printing Number of cities
+    
+    for i,(city, ) in enumerate(cities):
+        print(i+1, city)
+    cityindex = int(input('\nEnter the city no. : '))-1
+    (buyercity, ) = cities[cityindex]
+    return (True, buyercity)
+    
+# main function of buy
+
+def buy(uid):
+    c.execute(f"SELECT city, state, buy_list FROM user WHERE uid = {uid}")
+    buyercity, buyerstate, buy_list = c.fetchone()
+    satisfied = False
+    
+    #checking if buyer's city has any products
+    
+    c.execute(f"SELECT pid from product WHERE uid = (SELECT uid FROM user WHERE city = '{buyercity}' AND uid != {uid})")
+    flag = not c.fetchone() == None
+    
+    #until user satisfied loop keeps on repeating
+    
+    while not satisfied:
+        print('\nCurrently your city is', buyercity)
+        changecity = input('If you want to change your city then type "Yes" otherwise type "No" : ')
+        if changecity[0] in 'yY':
+            flag, buyercity = change_city(uid, buyercity, buyerstate)
+         
+        #Products available for his home state
+        
+        if flag:
+            print('\nproducts for city', buyercity)
+            c.execute(f"SELECT user.uid, product.pid, product.Product_Title, product.Product_Desc, product.dop, product.mrp, product.sp FROM user INNER JOIN product on user.uid = product.uid WHERE user.city = '{buyercity}'")
+            products = c.fetchall()
+            for i, product in enumerate(products):
+                _, _, Product_Title, Product_Desc, dop, mrp, sp = product
+                print('\nProduct',i+1)
+                print('-'*10)
+                print(f"""Product\t\t\t{Product_Title}
+                    \nDescription\t\t{Product_Desc}
+                    \nPurchased Date\t\t{dop}
+                    \nMarked Price\t\t{mrp}
+                    \nSelling Price\t\t{sp}
+                """)
+                
+            #if user wants to purchase product
+            
+            productPurchase = input('\n\nif you want to purchase any product type "Yes" else "No" : ')
+            if productPurchase[0] in 'yY':
+                productindex = int(input('\nEnter the Product no. which you want to buy : '))-1
+                uid_sell, pid = products[productindex][0:2]
+                
+                #Seller's information
+                
+                c.execute(f"SELECT FirstName, LastName, email, Mobile_Number FROM user WHERE uid = {uid_sell}")
+                fname, lname, email, MobNo = c.fetchone()
+                print("\nContact the seller to purchase the product and paying process")
+                print("-"*86)
+                print(f"""\nSeller Name\t\t{fname} {lname}
+                        \nMobile Number\t\t{MobNo}
+                        \nEmail\t\t\t{email}""")
+                
+                # updating users who wants to purchase this product in the buyer list
+                
+                c.execute(f"SELECT buyer_list FROM product WHERE pid = {pid}")
+                (buyer_list, ) = c.fetchone()
+                print('buyer_list', buyer_list)
+                buyer_list_changed = ','.join(set(buyer_list.split(',')+[str(uid)]))
+                c.execute(f"UPDATE product SET buyer_list = '{buyer_list_changed}' WHERE pid = {pid}")
+                
+                # updating buyer's buy list which product buyer wants to purchase
+                
+                buy_list_changed = ','.join(set(buy_list.split(',')+[str(pid)]))
+                c.execute(f"UPDATE user SET buy_list = '{buy_list_changed}' WHERE uid = {uid}")
+                
+                #commit changes
+                conn.commit()
+        else:
+            print(f'\ncity {buyercity} has no products to sell')
+        satisfied = input('Are you satisfied type "Yes"/"No" : ')[0] in "yY"
+    c.close()
+
+if __name__ == '__main__':
+    buy(1)
