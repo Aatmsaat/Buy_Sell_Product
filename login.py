88499403f56693f9ba1a2cae9ba52c985b9ff201
Vaibhav Gupta <47090589+Aatmsaat@users.noreply.github.com>
2021-01-05 20:24:47 +0530

    just added validation and some changes
    validation of email, password and mobile number etc.

--- login.py
+++ login.py
@@ -1,79 +1,97 @@
 import sqlite3
+import re
 
-conn = sqlite3.connect(':memory:')
+conn = sqlite3.connect('my_database.db')
 
 c = conn.cursor()
-
-c.execute("""CREATE TABLE user(
-    uid integer PRIMARY KEY AUTOINCREMENT,
-    FirstName text,
-    LastName text,
-    Moblie_Number integer,
-    email text,
-    password text,
-    city text,
-    state text)""")
-
-c.execute("INSERT INTO user(FirstName, LastName, Moblie_Number, email, password, city, state) VALUES ('Vaibhav', 'Gupta', 17546545645, 'dsfs@gmail.com', 'vai12345','lucknow', 'Uttar Pradesh')")
-c.execute("INSERT INTO user(FirstName, LastName, Moblie_Number, email, password, city, state) VALUES ('Aatmsaat', 'Gupta', 17546545645, 'dsfs@gmail.com', 'aat12345', 'kanpur', 'Uttar Pradesh')")
-c.execute("INSERT INTO user(FirstName, LastName, Moblie_Number, email, password, city, state) VALUES ('bittu', 'singh', 17546545645, 'dsfs@gmail.com', 'bit12345','alahabaad', 'Uttar Pradesh')")
-conn.commit()
 
 # Function to create a new user account
 def signup():
     print("\nPlease enter the following detail")
-    fname = input("Enter first name: ")
-    lname = input("Enter last name: ")
-    mob = int(input("Enter mobile number: "))
-    email = input("Enter email: ")
-    pasw = input("Enter password: ")
-    city = input("Enter city: ")
-    state = input("Enter state: ")
-    var_insert = []
-    var_insert.append(fname)
-    var_insert.append(lname)
-    var_insert.append(mob)
-    var_insert.append(email)
-    var_insert.append(pasw)
-    var_insert.append(city)
-    var_insert.append(state)
-    with sqlite3.connect(':memory:') as db:
-        cursor = db.cursor()
-        cursor.execute("INSERT INTO user(FirstName, LastName, Moblie_Number, email, password, city, state) VALUES (?,?,?,?,?,?,?);", var_insert)
-        print("Inserted")
-        db.commit()
-
-
+    
+    fname, lname, mob, email, pasw, city, state = [None]*7
+    
+    # validation and inputs
+    
+    while True:
+        fname = input("Enter first name: ")
+        if fname.strip().count(' ') == 0:
+            break
+        print('Enter only your first name!')
+        
+    while True:
+        lname = input("Enter last name: ")
+        if lname.strip().count(' ') == 0:
+            break
+        print('Enter only your last name!')
+    
+    while True:
+        mob = input("Enter mobile number: ")
+        mob_len = len(mob)
+        if mob_len == 10:
+            mob = int(mob)
+            break
+        print('mobile number must be of only 10 digits')
+            
+    while True:
+        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        email = input("Enter email: ")
+        if(re.search(regex,email)):
+            c.execute('SELECT uid FROM user WHERE email = "{email}"')
+            if c.fetchone():
+                print('This Email has already account!')
+            else:
+                break
+        else:
+            print('invalid email!')
+        
+    while True:
+        print('For password')
+        print('length should be at least 6')
+        print('length should be not be greater than 20')
+        print('Password should have at least one numeral') 
+        print('Password should have at least one uppercase letter') 
+        print('Password should have at least one lowercase letter') 
+        print('Password should have at least one of the symbols $@#')
+        pasw = input("Enter password: ")
+        reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+        pat = re.compile(reg) 
+        if re.search(pat, pasw):
+            break
+        print('Invalid password!')
+    
+    state = input("Enter full name of state: ").upper()
+    city = input("Enter full name of city: ").upper()
+    
+    c.execute("INSERT INTO user(FirstName, LastName, Mobile_Number, email, password, state, city) VALUES (?, ?, ?, ?, ?, ?, ?)", (fname, lname, mob, email, pasw, state, city))
+    conn.commit()
+    print('Welcome ', fname, lname)
+    c.execute(f"SELECT uid FROM user WHERE email = '{email}'")
+    (uid, ) = c.fetchone()
+    return uid
+   
 # Function for login system
+
 def login():
     print("Please enter the login detail\n")
-    email = input("Email: ")
-    pasw = input("Password: ")
-    var_select = []
-    var_select.append(email)
-    var_select.append(pasw)
-    with sqlite3.connect(':memory:') as db:
-        cursor = db.cursor()
-        sql = 'SELECT * FROM user WHERE email = ? AND passsword = ?';
-        cursor.execute(sql, var_select)
-        result = cursor.fetchall()
-        for row in result:
-            print(str(row))
+    while True:
+        email = input("Email: ")
+        pasw = input("Password: ")
+        var_select = []
+        var_select.append(email)
+        var_select.append(pasw)
+        sql = 'SELECT uid, FirstName, LastName FROM user WHERE email = ? AND password = ?'
+        c.execute(sql, var_select)
+        result = c.fetchone()
+        if result:
+            uid, fname, lname = result
+            print('Welcome', fname, lname)
+            return uid
+        else:
+            print('Either invalid email/password ')
 
-# Startup of program to execute
-while True:
-    print("User Login consol\n1.Signup\n2.Login\n3.Exit")
-    n = int(input())
-    if(n==1):
-        signup()
-    elif(n==2):
-        login()
-    elif(n==3):
-        break
-    else:
-        print("Wrong Input")
-
-
-
-
+# testing functions
     
+if __name__ == '__main__':
+    print(signup())
+    print(login())
